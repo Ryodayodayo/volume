784d78602ffc79b539d3adea7ec2f30d7beb7cbc
Ryodayodayo <mitosara10@gmail.com>
2025-05-30 06:38:42 +0900

    test.pyのapp.runを変更

--- backend/test.py
+++ backend/test.py
@@ -120,4 +120,5 @@ def download_file():
     return 0
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    #app.run(debug=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
